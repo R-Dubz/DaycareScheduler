2a4eb7d54c2af9a9ad776d4743a4930684ab4526
R-Dubz <whiterm@clarkson.edu>
2016-09-21 15:54:40 -0400

    test
    testing

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile('/project.html', {root: __dirname })
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
