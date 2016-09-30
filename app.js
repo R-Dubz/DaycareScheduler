8f44505e03e78ff230175e120a1e15c44058c129
DanielMBurns <damburns@clarkson.edu>
2016-09-30 11:45:23 -0400

    changes
    changes made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use('/html', express.static(__dirname, '/Project1'));
 
 app.get('/', function (req, res) {
     res.sendFile('/project.html', {root: __dirname })
