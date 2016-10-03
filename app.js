c7b4d769e2c9ce8ca320c729ebadacbad71b3079
DanielMBurns <damburns@clarkson.edu>
2016-10-03 14:06:19 -0400

    The html pages are in Project. To run type localhost:3000/Home.html

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
 
-//app.use('/html', express.static(__dirname, '/Project1'));
+app.use(express.static(__dirname + '/Project'));
+
+app.use(express.static(__dirname + '/'));
+
 
 app.get('/', function (req, res) {
-    res.sendFile('/project.html', {root: __dirname })
+    res.sendFile('project.html');
+//	app.use('html',express.static(path.join(__dirname, 'Project/Home.html')));
+//	res.sendFile('/Home.html', {root: __dirname })
 });
 
-
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.get('/Home',function(req,res){
+  res.sendFile('/Home.html');
 });
+
+app.listen(3000);
+console.log("running at port 3000");
+//app.get('/sitemap',function(req,res){
+ // res.sendFile(path.join(__dirname+'/sitemap.html'));
+//});
+
+//app.listen(3000);
+
+//console.log("Running at Port 3000");
+
+//app.listen(3001, function () {
+ // console.log('Example app listening on port 3001!   \n' + __dirname);
+//});
