3c41edef91f8db5ecffec6165e65bab2ba8057d7
R-Dubz <whiterm@clarkson.edu>
2016-11-06 16:08:33 -0500

    minor changes

---
+++ Source/Client/Controllers/testController.js
@@ -0,0 +1,9 @@
+// app.controller('testCtrl', function($scope, $http) {
+
+//     var testCall = function() {
+//         $http.get("./Source/Server/quickstart.js").then(function(response) {
+//             console.log("Call Success");
+//         });
+//     }
+// });
+
