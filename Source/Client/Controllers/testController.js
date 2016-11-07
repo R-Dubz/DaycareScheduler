0f8bdd1e8eb26216512ea62f45f61d0c0eca9d30
R-Dubz <whiterm@clarkson.edu>
2016-11-06 23:38:13 -0500

    minor changes to controller and WL template

--- Source/Client/Controllers/testController.js
+++ Source/Client/Controllers/testController.js
@@ -1,7 +1,8 @@
 angular.module('DaycareApp').controller('testController', ['$scope', '$http', function($scope, $http){
 
     $scope.test = function() {
-        console.log("Hello World!");
+        // console.log("Hello World!");
+        alert("An HTTP request has been sent to the server.\nThis request will check the google sheet for new entires and push them to the database.");
     };
 
 }]);
