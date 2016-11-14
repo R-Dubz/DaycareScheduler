5ace47be7a0a65cc40707780c8cf18b4e4b53aaa
DanielMBurns <damburns@clarkson.edu>
2016-11-14 12:11:02 -0500

    I made some changes to main.css and all the html pages that keeps the dropdown menu in the top right corner

---
+++ Source/Client/Controllers/EnrollmentController.js
@@ -0,0 +1,43 @@
+angular.module('DaycareApp').controller('EnrollmentController', ['$scope', '$http', function($scope, $http){
+
+        $scope.Children = [];
+        $scope.Profile = [];        
+        
+        $scope.RefreshDatabase = function() {
+            $http.get('/RefreshDatabase')
+            .then(function(response) {
+                alert("An HTTP request has been sent to the server.\nNow updating DaycareDB.db!");
+                // location.reload();
+            });
+        };
+
+        $scope.LoadWaitingList = function() {
+            $http.get('/LoadWaitingList')
+            .then(function(response) {
+                // alert("HTTP request set, getting data");
+                // $scope.Children.push(response.data);
+
+                for(var i = 0; i < response.data.length; i++){
+                    $scope.Children.push(response.data[i]);
+                }
+
+            });
+        };
+
+        $scope.storeProfile = function(child){
+            $http.post('/storeTempProfile', child)
+            .then(function(response) {
+                window.location.href = 'DemoPage.html';
+            });
+        }
+
+        $scope.LoadTempProfile = function() {
+            $http.get('/getTempProfile')
+            .then(function(response) {
+                // alert("HTTP request set, getting data");
+                // $scope.Children.push(response.data);
+                $scope.Profile.push(response);
+            });
+        };
+
+    }]);
