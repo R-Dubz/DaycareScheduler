4e942b4e604ffb5c2c22b35533f02f91604cb089
R-Dubz <whiterm@clarkson.edu>
2016-11-15 15:10:02 -0500

    Profile is slowly becoming editable

--- Source/Client/Controllers/ProfileController.js
+++ Source/Client/Controllers/ProfileController.js
@@ -1,14 +1,19 @@
-    angular.module('DaycareApp').controller('ProfileController', ['$scope', '$http', function($scope, $http){
+angular.module('DaycareApp').controller('ProfileController', ['$scope', '$http', function($scope, $http){
 
-        $scope.Profile = [];        
+    $scope.Profile = [];      
+    $scope.Editing = false;  
 
-        $scope.LoadTempProfile = function() {
-            $http.get('/getTempProfile')
-            .then(function(response) {
-                // alert("HTTP request set, getting data");
-                // $scope.Children.push(response.data);
-                $scope.Profile.push(response.data[0]);
-            });
-        };
+    $scope.LoadTempProfile = function() {
+        $http.get('/getTempProfile')
+        .then(function(response) {
+            // alert("HTTP request set, getting data");
+            // $scope.Children.push(response.data);
+            $scope.Profile.push(response.data[0]);
+        });
+    };
 
-    }]);
+    $scope.EditProfile = function(){
+        $scope.Editing = !$scope.Editing
+    };
+
+}]);
