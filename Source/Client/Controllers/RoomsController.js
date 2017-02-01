33add301dfeab9ad044073976fa04010d785757a
R-Dubz <whiterm@clarkson.edu>
2017-02-01 00:56:44 -0500

    Worked on Classrooms page

--- Source/Client/Controllers/RoomsController.js
+++ Source/Client/Controllers/RoomsController.js
@@ -1,11 +1,17 @@
 angular.module('DaycareApp').controller('RoomsController', ['$scope', '$http', function($scope, $http){
 
         $scope.Children = [];
-        $scope.Profile = [];  
-        $scope.sortType = 'ChildName'; 
-        $scope.sortReverse = false;
-        $scope.searchText1 = 'School Age';     
-        $scope.searchText = 'Toddler1';  
+        $scope.Profile = [];   
+        $scope.sortType = 'jsFriendlyTimeStamp'; 
+        $scope.sortReverse = true;
+        $scope.searchText = ''; 
+        $scope.ShowModal = true;  
+        $scope.names = ["Infant Room", "Toddler 1", "Toddler 2", "Preschool 1", "Preschool 2", "Progressive", "School Age", "Classroom 8"];
+
+        $scope.myFunc = function(item) {
+            alert(item);
+            $scope.Children = [];
+        };   
 
 
         $scope.callEnrolledList = function() {
@@ -38,20 +44,156 @@ angular.module('DaycareApp').controller('RoomsController', ['$scope', '$http', f
             });
         };
 
-        $scope.storeProfile = function(child){
-            $http.post('/storeTempProfile', child)
-            .then(function(response) {
-                window.location.href = 'EnrolledDemoPage.html';
-            });
-        }
 
-        $scope.LoadTempProfile = function() {
-            $http.get('/getTempProfile')
+    /* MODAL JUNK */
+
+    $scope.OpenModal = function() {
+        $scope.ShowModal = true;
+        document.getElementById('modalContent').style.width = "270px";
+    }
+
+    $scope.CloseModal = function() {
+        $scope.ShowModal = false;
+        $scope.numOfRows = 1;
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {  
+        if (event.target == modal) {
+            // $scope.ShowModal = false;
+            controllerFunction.CloseModal();
+        }
+        controllerFunction.$apply(); // This makes it so the page "sees" that we changed the variable.
+    }
+
+
+        // $scope.storeProfile = function(child){
+        //     $http.post('/storeTempProfile', child)
+        //     .then(function(response) {
+        //         window.location.href = 'EnrolledDemoPage.html';
+        //     });
+        // }
+
+        // $scope.LoadTempProfile = function() {
+        //     $http.get('/getTempProfile')
+        //     .then(function(response) {
+        //         // alert("HTTP request set, getting data");
+        //         // $scope.Children.push(response.data);
+        //         $scope.Profile.push(response);
+        //     });
+        // };
+
+
+
+
+
+
+
+
+
+
+        $scope.LoadWaitingList = function() {
+            $http.get('/LoadWaitingList')
             .then(function(response) {
                 // alert("HTTP request set, getting data");
                 // $scope.Children.push(response.data);
-                $scope.Profile.push(response);
+
+                for(var i = 0; i < response.data.length; i++){
+                    response.data[i].jsFriendlyBirthDate = new Date(response.data[i].ChildBirthdate);
+                    response.data[i].jsFriendlyTimeStamp = new Date(response.data[i].TimeStamp);
+                    response.data[i].jsFriendlyDesiredEnrollment = new Date(response.data[i].DesiredEnrollment);      
+
+
+                    if((response.data[i].GuardianStatus1 === "Full-time SUNY student") || 
+                    (response.data[i].GuardianStatus1 === "Part-time SUNY student") || 
+                    (response.data[i].GuardianStatus2 === "Full-time SUNY student") || 
+                    (response.data[i].GuardianStatus2 === "Part-time SUNY student")){
+                        response.data[i].Priority = 4;
+                        response.data[i].PriorityText = "High";                        
+                    } else if(
+                        (response.data[i].GuardianStatus1 === "UUP") || 
+                        (response.data[i].GuardianStatus1 === "CSEA")|| 
+                        (response.data[i].GuardianStatus1 === "PEF") || 
+                        (response.data[i].GuardianStatus1 === "NYSCOPBA") ||
+                        (response.data[i].GuardianStatus2 === "UUP") || 
+                        (response.data[i].GuardianStatus2 === "CSEA")|| 
+                        (response.data[i].GuardianStatus2 === "PEF") || 
+                        (response.data[i].GuardianStatus2 === "NYSCOPBA")){
+                        response.data[i].Priority = 3;
+                        response.data[i].PriorityText = "Medium";    
+                    } else if((response.data[i].GuardianStatus1 === "Other State") || 
+                    (response.data[i].GuardianStatus2 === "Other State")){
+                        response.data[i].Priority = 2;
+                        response.data[i].PriorityText = "Low";                        
+                    } else {
+                        response.data[i].Priority = 1;
+                        response.data[i].PriorityText = "None";   
+                    }
+
+                    if((response.data[i].GuardianStatus1 === "Full-time SUNY student") || 
+                    (response.data[i].GuardianStatus1 === "Part-time SUNY student") || 
+                    (response.data[i].GuardianStatus2 === "Full-time SUNY student") || 
+                    (response.data[i].GuardianStatus2 === "Part-time SUNY student")){
+                        response.data[i].Priority = 4;
+                        response.data[i].PriorityText = "High";                        
+                    } else if(
+                        (response.data[i].GuardianStatus1 === "UUP") || 
+                        (response.data[i].GuardianStatus1 === "CSEA")|| 
+                        (response.data[i].GuardianStatus1 === "PEF") || 
+                        (response.data[i].GuardianStatus1 === "NYSCOPBA") ||
+                        (response.data[i].GuardianStatus2 === "UUP") || 
+                        (response.data[i].GuardianStatus2 === "CSEA")|| 
+                        (response.data[i].GuardianStatus2 === "PEF") || 
+                        (response.data[i].GuardianStatus2 === "NYSCOPBA")){
+                        response.data[i].Priority = 3;
+                        response.data[i].PriorityText = "Medium";    
+                    } else if((response.data[i].GuardianStatus1 === "Other State") || 
+                    (response.data[i].GuardianStatus2 === "Other State")){
+                        response.data[i].Priority = 2;
+                        response.data[i].PriorityText = "Low";                        
+                    } else {
+                        response.data[i].Priority = 1;
+                        response.data[i].PriorityText = "None";   
+                    }
+
+                    var ChildBirthday = response.data[i].jsFriendlyBirthDate;
+                    var currentTime = new Date();
+                    var diff = currentTime - ChildBirthday;
+                    var age = Math.floor(diff/31557600000);
+
+                    response.data[i].AgeGroup = age;
+
+                    // if(age < 1){
+                    //     response.data[i].AgeGroup = "IN";
+                    // } else if(age >= 1 && age < 2){
+
+                    // } else if( age >= 3 && age <4){
+
+                    // } else if( age >= 3 && age <4){
+
+                    // } else
+
+
+
+
+
+                    $scope.Children.push(response.data[i]);
+                    // $scope.Children[i].push(jsFriendlyBirthDate);                    
+                }
+
+                if($scope.Children.length === 0){
+                alert("It appears there are no children on the waiting list.\nPlease refresh the database and reload the page.");                
+                }
+
             });
         };
+
+
+
+
+
+
+
+
 
     }]);
