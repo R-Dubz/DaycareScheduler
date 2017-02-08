31ee8874224dfe81bc7ec8c5777f17d227f8c6a0
R-Dubz <whiterm@clarkson.edu>
2017-02-07 20:21:34 -0500

    Modal fixes and rooms development

--- Source/Client/Controllers/RoomsController.js
+++ Source/Client/Controllers/RoomsController.js
@@ -7,10 +7,47 @@ angular.module('DaycareApp').controller('RoomsController', ['$scope', '$http', f
         $scope.searchText = ''; 
         $scope.ShowModal = false;  
         $scope.names = ["Infant Room", "Toddler 1", "Toddler 2", "Preschool 1", "Preschool 2", "Progressive", "School Age", "Classroom 8"];
+        $scope.ClassroomList = [];
 
-        $scope.myFunc = function(item) {
-            alert(item);
+        $scope.myFunc = function(target) {
             $scope.Children = [];
+            alert(target);            
+
+            if(target === "Infant Room"){
+                target = "InfantRoom";
+            } else if(target === "Toddler 1"){
+                target = "Toddler1"; 
+            } else if(target === "Toddler 2"){
+                target = "Toddler2"; 
+            } else if(target === "Preschool 1"){
+                target = "Preschool3"; 
+            } else if(target === "Preschool 2"){
+                target = "Preschool4"; 
+            } else if(target === "School Age"){
+                target = "SchoolAge"; 
+            } else if(target === "Classroom 8"){
+                target = "Classroom8"; 
+            } else {
+                //do nothing
+            }
+
+            targetClassroom = {Classroom: target}; 
+
+            $http.post('/callAllClass', targetClassroom)
+            .then(function(response) {
+                $scope.ClassroomList = response.data;
+                $scope.ClassroomList.forEach(function(element) {
+                    $http({
+                        url: '/getChildInfo',
+                        method: "POST",
+                        data: { 'ChildID': element.ChildID }
+                    })
+                    .then(function(response) {
+                        element.ChildName = response.data[0].ChildName;
+                        element.ChildBirthdate = response.data[0].ChildBirthdate;                        
+                    });
+                });
+            });
         };   
 
 
@@ -66,124 +103,24 @@ angular.module('DaycareApp').controller('RoomsController', ['$scope', '$http', f
     }
 
 
-        // $scope.storeProfile = function(child){
-        //     $http.post('/storeTempProfile', child)
-        //     .then(function(response) {
-        //         window.location.href = 'EnrolledDemoPage.html';
-        //     });
-        // }
 
-        // $scope.LoadTempProfile = function() {
-        //     $http.get('/getTempProfile')
-        //     .then(function(response) {
-        //         // alert("HTTP request set, getting data");
-        //         // $scope.Children.push(response.data);
-        //         $scope.Profile.push(response);
-        //     });
-        // };
+        $scope.LoadRoomList = function(target) {
+            targetClassroom = {Classroom: target}; 
 
-
-
-
-
-
-
-
-
-
-        $scope.LoadWaitingList = function() {
-            $http.get('/LoadWaitingList')
+            $http.post('/callAllClass', targetClassroom)
             .then(function(response) {
-                // alert("HTTP request set, getting data");
-                // $scope.Children.push(response.data);
-
-                for(var i = 0; i < response.data.length; i++){
-                    response.data[i].jsFriendlyBirthDate = new Date(response.data[i].ChildBirthdate);
-                    response.data[i].jsFriendlyTimeStamp = new Date(response.data[i].TimeStamp);
-                    response.data[i].jsFriendlyDesiredEnrollment = new Date(response.data[i].DesiredEnrollment);      
-
-
-                    if((response.data[i].GuardianStatus1 === "Full-time SUNY student") || 
-                    (response.data[i].GuardianStatus1 === "Part-time SUNY student") || 
-                    (response.data[i].GuardianStatus2 === "Full-time SUNY student") || 
-                    (response.data[i].GuardianStatus2 === "Part-time SUNY student")){
-                        response.data[i].Priority = 4;
-                        response.data[i].PriorityText = "High";                        
-                    } else if(
-                        (response.data[i].GuardianStatus1 === "UUP") || 
-                        (response.data[i].GuardianStatus1 === "CSEA")|| 
-                        (response.data[i].GuardianStatus1 === "PEF") || 
-                        (response.data[i].GuardianStatus1 === "NYSCOPBA") ||
-                        (response.data[i].GuardianStatus2 === "UUP") || 
-                        (response.data[i].GuardianStatus2 === "CSEA")|| 
-                        (response.data[i].GuardianStatus2 === "PEF") || 
-                        (response.data[i].GuardianStatus2 === "NYSCOPBA")){
-                        response.data[i].Priority = 3;
-                        response.data[i].PriorityText = "Medium";    
-                    } else if((response.data[i].GuardianStatus1 === "Other State") || 
-                    (response.data[i].GuardianStatus2 === "Other State")){
-                        response.data[i].Priority = 2;
-                        response.data[i].PriorityText = "Low";                        
-                    } else {
-                        response.data[i].Priority = 1;
-                        response.data[i].PriorityText = "None";   
-                    }
-
-                    if((response.data[i].GuardianStatus1 === "Full-time SUNY student") || 
-                    (response.data[i].GuardianStatus1 === "Part-time SUNY student") || 
-                    (response.data[i].GuardianStatus2 === "Full-time SUNY student") || 
-                    (response.data[i].GuardianStatus2 === "Part-time SUNY student")){
-                        response.data[i].Priority = 4;
-                        response.data[i].PriorityText = "High";                        
-                    } else if(
-                        (response.data[i].GuardianStatus1 === "UUP") || 
-                        (response.data[i].GuardianStatus1 === "CSEA")|| 
-                        (response.data[i].GuardianStatus1 === "PEF") || 
-                        (response.data[i].GuardianStatus1 === "NYSCOPBA") ||
-                        (response.data[i].GuardianStatus2 === "UUP") || 
-                        (response.data[i].GuardianStatus2 === "CSEA")|| 
-                        (response.data[i].GuardianStatus2 === "PEF") || 
-                        (response.data[i].GuardianStatus2 === "NYSCOPBA")){
-                        response.data[i].Priority = 3;
-                        response.data[i].PriorityText = "Medium";    
-                    } else if((response.data[i].GuardianStatus1 === "Other State") || 
-                    (response.data[i].GuardianStatus2 === "Other State")){
-                        response.data[i].Priority = 2;
-                        response.data[i].PriorityText = "Low";                        
-                    } else {
-                        response.data[i].Priority = 1;
-                        response.data[i].PriorityText = "None";   
-                    }
-
-                    var ChildBirthday = response.data[i].jsFriendlyBirthDate;
-                    var currentTime = new Date();
-                    var diff = currentTime - ChildBirthday;
-                    var age = Math.floor(diff/31557600000);
-
-                    response.data[i].AgeGroup = age;
-
-                    // if(age < 1){
-                    //     response.data[i].AgeGroup = "IN";
-                    // } else if(age >= 1 && age < 2){
-
-                    // } else if( age >= 3 && age <4){
-
-                    // } else if( age >= 3 && age <4){
-
-                    // } else
-
-
-
-
-
-                    $scope.Children.push(response.data[i]);
-                    // $scope.Children[i].push(jsFriendlyBirthDate);                    
-                }
-
-                if($scope.Children.length === 0){
-                alert("It appears there are no children on the waiting list.\nPlease refresh the database and reload the page.");                
-                }
-
+                $scope.ClassroomList = response.data;
+                $scope.ClassroomList.forEach(function(element) {
+                    $http({
+                        url: '/getChildInfo',
+                        method: "POST",
+                        data: { 'ChildID': element.ChildID }
+                    })
+                    .then(function(response) {
+                        element.ChildName = response.data[0].ChildName;
+                        element.ChildBirthdate = response.data[0].ChildBirthdate;                        
+                    });
+                });
             });
         };
 
--- Source/Client/Controllers/WaitingProfileController.js
+++ Source/Client/Controllers/WaitingProfileController.js
@@ -55,63 +55,102 @@ angular.module('DaycareApp').controller('WaitingProfileController', ['$scope', '
            (document.getElementById('ThursdayIn').value === "null" && document.getElementById('ThursdayOut').value !== "null") ||
            (document.getElementById('ThursdayIn').value !== "null" && document.getElementById('ThursdayOut').value === "null") ||
            (document.getElementById('FridayIn').value === "null" && document.getElementById('FridayOut').value !== "null") ||
-           (document.getElementById('FridayIn').value !== "null" && document.getElementById('FridayOut').value === "null")
-           (document.getElementById('MondayIn2').value === "null" && document.getElementById('MondayOut2').value !== "null") ||
-           (document.getElementById('MondayIn2').value !== "null" && document.getElementById('MondayOut2').value === "null") ||
-           (document.getElementById('TuesdayIn2').value === "null" && document.getElementById('TuesdayOut2').value !== "null") ||
-           (document.getElementById('TuesdayIn2').value !== "null" && document.getElementById('TuesdayOut2').value === "null") ||
-           (document.getElementById('WednesdayIn2').value === "null" && document.getElementById('WednesdayOut2').value !== "null") ||
-           (document.getElementById('WednesdayIn2').value !== "null" && document.getElementById('WednesdayOut2').value === "null") ||
-           (document.getElementById('ThursdayIn2').value === "null" && document.getElementById('ThursdayOut2').value !== "null") ||
-           (document.getElementById('ThursdayIn2').value !== "null" && document.getElementById('ThursdayOut2').value === "null") ||
-           (document.getElementById('FridayIn2').value === "null" && document.getElementById('FridayOut2').value !== "null") ||
-           (document.getElementById('FridayIn2').value !== "null" && document.getElementById('FridayOut2').value === "null")
-           (document.getElementById('MondayIn3').value === "null" && document.getElementById('MondayOut3').value !== "null") ||
-           (document.getElementById('MondayIn3').value !== "null" && document.getElementById('MondayOut3').value === "null") ||
-           (document.getElementById('TuesdayIn3').value === "null" && document.getElementById('TuesdayOut3').value !== "null") ||
-           (document.getElementById('TuesdayIn3').value !== "null" && document.getElementById('TuesdayOut3').value === "null") ||
-           (document.getElementById('WednesdayIn3').value === "null" && document.getElementById('WednesdayOut3').value !== "null") ||
-           (document.getElementById('WednesdayIn3').value !== "null" && document.getElementById('WednesdayOut3').value === "null") ||
-           (document.getElementById('ThursdayIn3').value === "null" && document.getElementById('ThursdayOut3').value !== "null") ||
-           (document.getElementById('ThursdayIn3').value !== "null" && document.getElementById('ThursdayOut3').value === "null") ||
-           (document.getElementById('FridayIn3').value === "null" && document.getElementById('FridayOut3').value !== "null") ||
-           (document.getElementById('FridayIn3').value !== "null" && document.getElementById('FridayOut3').value === "null")){
+           (document.getElementById('FridayIn').value !== "null" && document.getElementById('FridayOut').value === "null")){
             alert("One of the days is missing an In/Out time. Please try again.");
             return;
+        }
+        if(document.getElementById('MondayIn2') !== null){
+            if((document.getElementById('MondayIn2').value === "null" && document.getElementById('MondayOut2').value !== "null") ||
+            (document.getElementById('MondayIn2').value !== "null" && document.getElementById('MondayOut2').value === "null") ||
+            (document.getElementById('TuesdayIn2').value === "null" && document.getElementById('TuesdayOut2').value !== "null") ||
+            (document.getElementById('TuesdayIn2').value !== "null" && document.getElementById('TuesdayOut2').value === "null") ||
+            (document.getElementById('WednesdayIn2').value === "null" && document.getElementById('WednesdayOut2').value !== "null") ||
+            (document.getElementById('WednesdayIn2').value !== "null" && document.getElementById('WednesdayOut2').value === "null") ||
+            (document.getElementById('ThursdayIn2').value === "null" && document.getElementById('ThursdayOut2').value !== "null") ||
+            (document.getElementById('ThursdayIn2').value !== "null" && document.getElementById('ThursdayOut2').value === "null") ||
+            (document.getElementById('FridayIn2').value === "null" && document.getElementById('FridayOut2').value !== "null") ||
+            (document.getElementById('FridayIn2').value !== "null" && document.getElementById('FridayOut2').value === "null")){
+                alert("One of the days is missing an In/Out time. Please try again.");
+                return;
+            }
+        }
+        if(document.getElementById('MondayIn3') !== null){
+            if((document.getElementById('MondayIn3').value === "null" && document.getElementById('MondayOut3').value !== "null") ||
+            (document.getElementById('MondayIn3').value !== "null" && document.getElementById('MondayOut3').value === "null") ||
+            (document.getElementById('TuesdayIn3').value === "null" && document.getElementById('TuesdayOut3').value !== "null") ||
+            (document.getElementById('TuesdayIn3').value !== "null" && document.getElementById('TuesdayOut3').value === "null") ||
+            (document.getElementById('WednesdayIn3').value === "null" && document.getElementById('WednesdayOut3').value !== "null") ||
+            (document.getElementById('WednesdayIn3').value !== "null" && document.getElementById('WednesdayOut3').value === "null") ||
+            (document.getElementById('ThursdayIn3').value === "null" && document.getElementById('ThursdayOut3').value !== "null") ||
+            (document.getElementById('ThursdayIn3').value !== "null" && document.getElementById('ThursdayOut3').value === "null") ||
+            (document.getElementById('FridayIn3').value === "null" && document.getElementById('FridayOut3').value !== "null") ||
+            (document.getElementById('FridayIn3').value !== "null" && document.getElementById('FridayOut3').value === "null")){
+                alert("One of the days is missing an In/Out time. Please try again.");
+                return;
+            } 
         }
 
         sendID.push(ID.ChildID);
         ID.Classroom = document.getElementById('Classroom').value;
         ID.MI1 = document.getElementById('MondayIn').value;
-        ID.MI2 = document.getElementById('MondayIn2').value;
-        ID.MI3 = document.getElementById('MondayIn3').value;
         ID.MO1 = document.getElementById('MondayOut').value;
-        ID.MO2 = document.getElementById('MondayOut2').value;
-        ID.MO3 = document.getElementById('MondayOut3').value;
         ID.TI1 = document.getElementById('TuesdayIn').value;
-        ID.TI2 = document.getElementById('TuesdayIn2').value;
-        ID.TI3 = document.getElementById('TuesdayIn3').value;
         ID.TO1 = document.getElementById('TuesdayOut').value;
-        ID.TO2 = document.getElementById('TuesdayOut2').value;
-        ID.TO3 = document.getElementById('TuesdayOut3').value;
         ID.WI1 = document.getElementById('WednesdayIn').value;
-        ID.WI2 = document.getElementById('WednesdayIn2').value;
-        ID.WI3 = document.getElementById('WednesdayIn3').value;
         ID.WO1 = document.getElementById('WednesdayOut').value;
-        ID.WO2 = document.getElementById('WednesdayOut2').value;
-        ID.WO3 = document.getElementById('WednesdayOut3').value;
         ID.THI1 = document.getElementById('ThursdayIn').value;
-        ID.THI2 = document.getElementById('ThursdayIn2').value;
-        ID.THI3 = document.getElementById('ThursdayIn3').value;
         ID.THO1 = document.getElementById('ThursdayOut').value;
-        ID.THO2 = document.getElementById('ThursdayOut2').value;
-        ID.THO3 = document.getElementById('ThursdayOut3').value;
         ID.FI1 = document.getElementById('FridayIn').value;
-        ID.FI2 = document.getElementById('FridayIn2').value;
-        ID.FI3 = document.getElementById('FridayIn3').value;
         ID.FO1 = document.getElementById('FridayOut').value;
-        ID.FO2 = document.getElementById('FridayOut2').value;
-        ID.FO3 = document.getElementById('FridayOut3').value;
+
+        if(document.getElementById('MondayIn2') !== null){
+            ID.MI2 = document.getElementById('MondayIn2').value;
+            ID.MO2 = document.getElementById('MondayOut2').value;
+            ID.TI2 = document.getElementById('TuesdayIn2').value;
+            ID.TO2 = document.getElementById('TuesdayOut2').value;
+            ID.WI2 = document.getElementById('WednesdayIn2').value;
+            ID.WO2 = document.getElementById('WednesdayOut2').value;
+            ID.THI2 = document.getElementById('ThursdayIn2').value;
+            ID.THO2 = document.getElementById('ThursdayOut2').value;
+            ID.FI2 = document.getElementById('FridayIn2').value;
+            ID.FO2 = document.getElementById('FridayOut2').value;
+        } else {
+            ID.MI2 = null;
+            ID.MO2 = null;
+            ID.TI2 = null;
+            ID.TO2 = null;
+            ID.WI2 = null;
+            ID.WO2 = null;
+            ID.THI2 = null;
+            ID.THO2 = null;
+            ID.FI2 = null;
+            ID.FO2 = null;
+        }
+
+        if(document.getElementById('MondayIn3') !== null){
+            ID.MI3 = document.getElementById('MondayIn3').value;
+            ID.MO3 = document.getElementById('MondayOut3').value;
+            ID.TI3 = document.getElementById('TuesdayIn3').value;
+            ID.TO3 = document.getElementById('TuesdayOut3').value;
+            ID.WI3 = document.getElementById('WednesdayIn3').value;
+            ID.WO3 = document.getElementById('WednesdayOut3').value;
+            ID.THI3 = document.getElementById('ThursdayIn3').value;
+            ID.THO3 = document.getElementById('ThursdayOut3').value;
+            ID.FI3 = document.getElementById('FridayIn3').value;
+            ID.FO3 = document.getElementById('FridayOut3').value;
+        } else {
+            ID.MI3 = null;
+            ID.MO3 = null;
+            ID.TI3 = null;
+            ID.TO3 = null;
+            ID.WI3 = null;
+            ID.WO3 = null;
+            ID.THI3 = null;
+            ID.THO3 = null;
+            ID.FI3 = null;
+            ID.FO3 = null;
+        }
+
         sendID.push(ID.Classroom);
         if(ID.Classroom !== "null"){
             $http.post('/InsertChildToClass', ID)
