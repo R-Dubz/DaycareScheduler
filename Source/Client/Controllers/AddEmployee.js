2dc765860e066c223538e795506bc7e8f844ae88
DanielMBurns <damburns@clarkson.edu>
2017-02-17 12:46:49 -0500

    changed the "modal" stuff in addEmployee

--- Source/Client/Controllers/AddEmployee.js
+++ Source/Client/Controllers/AddEmployee.js
@@ -30,7 +30,8 @@ angular.module('DaycareApp').controller('AddEmployee', ['$scope', '$http', funct
 
 }]);
 
-$scope.EditClassroomInfo = function(ID){
+$scope.acceptChild = function(ID){
+        var sendID = [];
 
         if((document.getElementById('MondayIn').value === "null" && document.getElementById('MondayOut').value !== "null") ||
            (document.getElementById('MondayIn').value !== "null" && document.getElementById('MondayOut').value === "null") ||
@@ -76,106 +77,65 @@ $scope.EditClassroomInfo = function(ID){
             } 
         }
 
-
-        var changes = {};
-        changes.ChildID = $scope.Profile[0].ChildID;
-        changes.oldClassroom = $scope.Profile[0].Classroom;
-        changes.Classroom = document.getElementById('Classroom').value;
-        changes.MI1 = document.getElementById('MondayIn').value;
-        changes.MO1 = document.getElementById('MondayOut').value;
-        changes.TI1 = document.getElementById('TuesdayIn').value;
-        changes.TO1 = document.getElementById('TuesdayOut').value;
-        changes.WI1 = document.getElementById('WednesdayIn').value;
-        changes.WO1 = document.getElementById('WednesdayOut').value;
-        changes.THI1 = document.getElementById('ThursdayIn').value;
-        changes.THO1 = document.getElementById('ThursdayOut').value;
-        changes.FI1 = document.getElementById('FridayIn').value;
-        changes.FO1 = document.getElementById('FridayOut').value;
+        sendID.push(ID.ChildID);
+        ID.Classroom = document.getElementById('Classroom').value;
+        ID.MI1 = document.getElementById('MondayIn').value;
+        ID.MO1 = document.getElementById('MondayOut').value;
+        ID.TI1 = document.getElementById('TuesdayIn').value;
+        ID.TO1 = document.getElementById('TuesdayOut').value;
+        ID.WI1 = document.getElementById('WednesdayIn').value;
+        ID.WO1 = document.getElementById('WednesdayOut').value;
+        ID.THI1 = document.getElementById('ThursdayIn').value;
+        ID.THO1 = document.getElementById('ThursdayOut').value;
+        ID.FI1 = document.getElementById('FridayIn').value;
+        ID.FO1 = document.getElementById('FridayOut').value;
 
         if(document.getElementById('MondayIn2') !== null){
-            changes.MI2 = document.getElementById('MondayIn2').value;
-            changes.MO2 = document.getElementById('MondayOut2').value;
-            changes.TI2 = document.getElementById('TuesdayIn2').value;
-            changes.TO2 = document.getElementById('TuesdayOut2').value;
-            changes.WI2 = document.getElementById('WednesdayIn2').value;
-            changes.WO2 = document.getElementById('WednesdayOut2').value;
-            changes.THI2 = document.getElementById('ThursdayIn2').value;
-            changes.THO2 = document.getElementById('ThursdayOut2').value;
-            changes.FI2 = document.getElementById('FridayIn2').value;
-            changes.FO2 = document.getElementById('FridayOut2').value;
+            ID.MI2 = document.getElementById('MondayIn2').value;
+            ID.MO2 = document.getElementById('MondayOut2').value;
+            ID.TI2 = document.getElementById('TuesdayIn2').value;
+            ID.TO2 = document.getElementById('TuesdayOut2').value;
+            ID.WI2 = document.getElementById('WednesdayIn2').value;
+            ID.WO2 = document.getElementById('WednesdayOut2').value;
+            ID.THI2 = document.getElementById('ThursdayIn2').value;
+            ID.THO2 = document.getElementById('ThursdayOut2').value;
+            ID.FI2 = document.getElementById('FridayIn2').value;
+            ID.FO2 = document.getElementById('FridayOut2').value;
         } else {
-            changes.MI2 = null;
-            changes.MO2 = null;
-            changes.TI2 = null;
-            changes.TO2 = null;
-            changes.WI2 = null;
-            changes.WO2 = null;
-            changes.THI2 = null;
-            changes.THO2 = null;
-            changes.FI2 = null;
-            changes.FO2 = null;
+            ID.MI2 = null;
+            ID.MO2 = null;
+            ID.TI2 = null;
+            ID.TO2 = null;
+            ID.WI2 = null;
+            ID.WO2 = null;
+            ID.THI2 = null;
+            ID.THO2 = null;
+            ID.FI2 = null;
+            ID.FO2 = null;
         }
 
         if(document.getElementById('MondayIn3') !== null){
-            changes.MI3 = document.getElementById('MondayIn3').value;
-            changes.MO3 = document.getElementById('MondayOut3').value;
-            changes.TI3 = document.getElementById('TuesdayIn3').value;
-            changes.TO3 = document.getElementById('TuesdayOut3').value;
-            changes.WI3 = document.getElementById('WednesdayIn3').value;
-            changes.WO3 = document.getElementById('WednesdayOut3').value;
-            changes.THI3 = document.getElementById('ThursdayIn3').value;
-            changes.THO3 = document.getElementById('ThursdayOut3').value;
-            changes.FI3 = document.getElementById('FridayIn3').value;
-            changes.FO3 = document.getElementById('FridayOut3').value;
+            ID.MI3 = document.getElementById('MondayIn3').value;
+            ID.MO3 = document.getElementById('MondayOut3').value;
+            ID.TI3 = document.getElementById('TuesdayIn3').value;
+            ID.TO3 = document.getElementById('TuesdayOut3').value;
+            ID.WI3 = document.getElementById('WednesdayIn3').value;
+            ID.WO3 = document.getElementById('WednesdayOut3').value;
+            ID.THI3 = document.getElementById('ThursdayIn3').value;
+            ID.THO3 = document.getElementById('ThursdayOut3').value;
+            ID.FI3 = document.getElementById('FridayIn3').value;
+            ID.FO3 = document.getElementById('FridayOut3').value;
         } else {
-            changes.MI3 = null;
-            changes.MO3 = null;
-            changes.TI3 = null;
-            changes.TO3 = null;
-            changes.WI3 = null;
-            changes.WO3 = null;
-            changes.THI3 = null;
-            changes.THO3 = null;
-            changes.FI3 = null;
-            changes.FO3 = null;
+            ID.MI3 = null;
+            ID.MO3 = null;
+            ID.TI3 = null;
+            ID.TO3 = null;
+            ID.WI3 = null;
+            ID.WO3 = null;
+            ID.THI3 = null;
+            ID.THO3 = null;
+            ID.FI3 = null;
+            ID.FO3 = null;
         }
 
-        if (document.getElementById('Classroom').value === "null") {
-            $http.post('/deleteChildFromClassroom', changes)
-            .then(function(response) {
-                alert("Child has been Removed from the classroom");
-                $scope.CloseModal();
-                window.location.reload(true); 
-            });
-        }
-        else if ($scope.Profile[0].Classroom === document.getElementById('Classroom').value) {
-            $http.post('/editChildClassroom', changes)
-            .then(function(response) {
-                alert("Child's classroom times have been changed");
-                $scope.CloseModal();
-                window.location.reload(true); 
-            });
-        }
-        else if($scope.Profile[0].Classroom === "" || $scope.Profile[0].Classroom === "null"){
-            $http.post('/InsertChildToClass', changes)
-            .then(function(response) {
-                alert("Child has been inserted into the classroom!"); 
-                $scope.CloseModal(); 
-                window.location.reload(true);     
-            });
-        }
-        else {
-            $http.post('/deleteChildFromClassroom', changes)
-            .then(function(response) {
-                alert("Child has been removed from the classroom. Beginning to move them to new classroom!");
-                $http.post('/InsertChildToClass', changes)
-                .then(function(response) {
-                    alert("Child has been inserted into the classroom!"); 
-                    $scope.CloseModal();  
-                    window.location.reload(true);    
-                });
-            })
-        }
-
-        SaveChanges();
     };
