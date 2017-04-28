3a0ce6268d6de0ecc1ae104854da482d8365d4d6
R-Dubz <whiterm@clarkson.edu>
2017-04-28 07:15:50 -0400

    Addemployee Form completely works now. Also modified employee profile to display times.

--- Source/Client/Controllers/AddEmployee.js
+++ Source/Client/Controllers/AddEmployee.js
@@ -13,7 +13,65 @@ angular.module('DaycareApp').controller('AddEmployee', ['$scope', '$http', funct
         Profile.EmailAddress = document.getElementById("EmailAddress").value;
         Profile.PhoneNumber = document.getElementById("PhoneNumber").value;
         Profile.PhoneNumber2 = document.getElementById("PhoneNumber2").value;
-                
+
+        Profile.MI1 = document.getElementById('MondayIn').value;
+        Profile.MO1 = document.getElementById('MondayOut').value;
+        Profile.TI1 = document.getElementById('TuesdayIn').value;
+        Profile.TO1 = document.getElementById('TuesdayOut').value;
+        Profile.WI1 = document.getElementById('WednesdayIn').value;
+        Profile.WO1 = document.getElementById('WednesdayOut').value;
+        Profile.THI1 = document.getElementById('ThursdayIn').value;
+        Profile.THO1 = document.getElementById('ThursdayOut').value;
+        Profile.FI1 = document.getElementById('FridayIn').value;
+        Profile.FO1 = document.getElementById('FridayOut').value;
+
+        if(document.getElementById('MondayIn2') !== null){
+            Profile.MI2 = document.getElementById('MondayIn2').value;
+            Profile.MO2 = document.getElementById('MondayOut2').value;
+            Profile.TI2 = document.getElementById('TuesdayIn2').value;
+            Profile.TO2 = document.getElementById('TuesdayOut2').value;
+            Profile.WI2 = document.getElementById('WednesdayIn2').value;
+            Profile.WO2 = document.getElementById('WednesdayOut2').value;
+            Profile.THI2 = document.getElementById('ThursdayIn2').value;
+            Profile.THO2 = document.getElementById('ThursdayOut2').value;
+            Profile.FI2 = document.getElementById('FridayIn2').value;
+            Profile.FO2 = document.getElementById('FridayOut2').value;
+        } else {
+            Profile.MI2 = null;
+            Profile.MO2 = null;
+            Profile.TI2 = null;
+            Profile.TO2 = null;
+            Profile.WI2 = null;
+            Profile.WO2 = null;
+            Profile.THI2 = null;
+            Profile.THO2 = null;
+            Profile.FI2 = null;
+            Profile.FO2 = null;
+        }
+
+        if(document.getElementById('MondayIn3') !== null){
+            Profile.MI3 = document.getElementById('MondayIn3').value;
+            Profile.MO3 = document.getElementById('MondayOut3').value;
+            Profile.TI3 = document.getElementById('TuesdayIn3').value;
+            Profile.TO3 = document.getElementById('TuesdayOut3').value;
+            Profile.WI3 = document.getElementById('WednesdayIn3').value;
+            Profile.WO3 = document.getElementById('WednesdayOut3').value;
+            Profile.THI3 = document.getElementById('ThursdayIn3').value;
+            Profile.THO3 = document.getElementById('ThursdayOut3').value;
+            Profile.FI3 = document.getElementById('FridayIn3').value;
+            Profile.FO3 = document.getElementById('FridayOut3').value;
+        } else {
+            Profile.MI3 = null;
+            Profile.MO3 = null;
+            Profile.TI3 = null;
+            Profile.TO3 = null;
+            Profile.WI3 = null;
+            Profile.WO3 = null;
+            Profile.THI3 = null;
+            Profile.THO3 = null;
+            Profile.FI3 = null;
+            Profile.FO3 = null;
+        }         
        
         var updates = Profile;
         $http.post('/addEmployee', updates)
--- Source/Client/Controllers/EmployeeProfileController.js
+++ Source/Client/Controllers/EmployeeProfileController.js
@@ -4,7 +4,7 @@ angular.module('DaycareApp').controller('EmployeeProfileController', ['$scope', 
     $scope.ShowNotesModal = false;
     $scope.ShowEditModal = false;    
 	$scope.CurrentEmployee = {};
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('myModal'); 
     var controllerFunction = $scope;
 
     $scope.backToList = function() {
@@ -14,6 +14,7 @@ angular.module('DaycareApp').controller('EmployeeProfileController', ['$scope', 
     $scope.LoadTempEmployeeProfile = function() {
         $http.get('/getTempEmployeeProfile')
         .then(function(response) {
+            $scope.ConvertTimesToStrings(response.data[0]);
             $scope.Profile.push(response.data[0]);
         });
     };
@@ -78,6 +79,142 @@ angular.module('DaycareApp').controller('EmployeeProfileController', ['$scope', 
         });
     }
 
+    $scope.ConvertTimesToStrings = function(employee){
+        if(employee.MondayIn !== "null"){
+            employee.MondayIn = $scope.timeTo12HrFormat(employee.MondayIn);
+            employee.MondayOut = $scope.timeTo12HrFormat(employee.MondayOut);
+        } else{
+            employee.MondayIn = ""
+            employee.MondayOut = "";
+        }
+        if(employee.MondayIn2 !== "null"){
+            employee.MondayIn2 = $scope.timeTo12HrFormat(employee.MondayIn2);
+            employee.MondayOut2 = $scope.timeTo12HrFormat(employee.MondayOut2);  
+        } else{
+            employee.MondayIn2 = ""
+            employee.MondayOut2 = "";
+        }
+        if(employee.MondayIn3 !== "null"){
+            employee.MondayIn3 = $scope.timeTo12HrFormat(employee.MondayIn3);
+            employee.MondayOut3 = $scope.timeTo12HrFormat(employee.MondayOut3);   
+        } else{
+            employee.MondayIn3 = ""
+            employee.MondayOut3 = "";
+        } 
+        if(employee.TuesdayIn !== "null"){
+            employee.TuesdayIn = $scope.timeTo12HrFormat(employee.TuesdayIn);
+            employee.TuesdayOut = $scope.timeTo12HrFormat(employee.TuesdayOut);
+        } else{
+            employee.TuesdayIn = ""
+            employee.TuesdayOut = "";
+        }   
+        if(employee.TuesdayIn2 !== "null"){
+            employee.TuesdayIn2 = $scope.timeTo12HrFormat(employee.TuesdayIn2);
+            employee.TuesdayOut2 = $scope.timeTo12HrFormat(employee.TuesdayOut2);  
+        } else{
+            employee.TuesdayIn2 = ""
+            employee.TuesdayOut2 = "";
+        }   
+        if(employee.TuesdayIn3 !== "null"){
+            employee.TuesdayIn3 = $scope.timeTo12HrFormat(employee.TuesdayIn3);
+            employee.TuesdayOut3 = $scope.timeTo12HrFormat(employee.TuesdayOut3);   
+        } else{
+            employee.TuesdayIn3 = ""
+            employee.TuesdayOut3 = "";
+        } 
+        if(employee.WednesdayIn !== "null"){
+            employee.WednesdayIn = $scope.timeTo12HrFormat(employee.WednesdayIn);
+            employee.WednesdayOut = $scope.timeTo12HrFormat(employee.WednesdayOut);
+        } else{
+            employee.WednesdayIn = ""
+            employee.WednesdayOut = "";
+        }   
+        if(employee.WednesdayIn2 !== "null"){
+            employee.WednesdayIn2 = $scope.timeTo12HrFormat(employee.WednesdayIn2);
+            employee.WednesdayOut2 = $scope.timeTo12HrFormat(employee.WednesdayOut2);  
+        } else{
+            employee.WednesdayIn2 = ""
+            employee.WednesdayOut2 = "";
+        }   
+        if(employee.WednesdayIn3 !== "null"){
+            employee.WednesdayIn3 = $scope.timeTo12HrFormat(employee.WednesdayIn3);
+            employee.WednesdayOut3 = $scope.timeTo12HrFormat(employee.WednesdayOut3);   
+        } else{
+            employee.WednesdayIn3 = ""
+            employee.WednesdayOut3 = "";
+        } 
+        if(employee.ThursdayIn !== "null"){
+            employee.ThursdayIn = $scope.timeTo12HrFormat(employee.ThursdayIn);
+            employee.ThursdayOut = $scope.timeTo12HrFormat(employee.ThursdayOut);
+        } else{
+            employee.ThursdayIn = ""
+            employee.ThursdayOut = "";
+        }   
+        if(employee.ThursdayIn2 !== "null"){
+            employee.ThursdayIn2 = $scope.timeTo12HrFormat(employee.ThursdayIn2);
+            employee.ThursdayOut2 = $scope.timeTo12HrFormat(employee.ThursdayOut2);  
+        } else{
+            employee.ThursdayIn2 = ""
+            employee.ThursdayOut2 = "";
+        }   
+        if(employee.ThursdayIn3 !== "null"){
+            employee.ThursdayIn3 = $scope.timeTo12HrFormat(employee.ThursdayIn3);
+            employee.ThursdayOut3 = $scope.timeTo12HrFormat(employee.ThursdayOut3);   
+        } else{
+            employee.ThursdayIn3 = ""
+            employee.ThursdayOut3 = "";
+        } 
+        if(employee.FridayIn !== "null"){
+            employee.FridayIn = $scope.timeTo12HrFormat(employee.FridayIn);
+            employee.FridayOut = $scope.timeTo12HrFormat(employee.FridayOut);
+        } else{
+            employee.FridayIn = ""
+            employee.FridayOut = "";
+        }   
+        if(employee.FridayIn2 !== "null"){
+            employee.FridayIn2 = $scope.timeTo12HrFormat(employee.FridayIn2);
+            employee.FridayOut2 = $scope.timeTo12HrFormat(employee.FridayOut2);  
+        } else{
+            employee.FridayIn2 = ""
+            employee.FridayOut2 = "";
+        }   
+        if(employee.FridayIn3 !== "null"){
+            employee.FridayIn3 = $scope.timeTo12HrFormat(employee.FridayIn3);
+            employee.FridayOut3 = $scope.timeTo12HrFormat(employee.FridayOut3);   
+        } else{
+            employee.FridayIn3 = ""
+            employee.FridayOut3 = "";
+        } 
+    };
+
+    $scope.timeTo12HrFormat = function(time) {   // Take a time in 24 hour format and format it in 12 hour format
+        var time_part_array = time.split(".");
+        var ampm = 'AM';
+
+        if (time_part_array[0] >= 12) {
+            ampm = 'PM';
+        }
+
+        if (time_part_array[0] > 12) {
+            time_part_array[0] = time_part_array[0] - 12;
+        }
+
+        if(time_part_array[1] == 0){
+            time_part_array[1] = "00";
+        } else if(time_part_array[1] == 25){
+            time_part_array[1] = "15";
+        } else if(time_part_array[1] == 5){
+            time_part_array[1] = "30";
+        } else if(time_part_array[1] == 75){
+            time_part_array[1] = "45";
+        } else {
+            time_part_array[1] = "00";
+        }
+
+        formatted_time = time_part_array[0] + ':' + time_part_array[1] + ' ' + ampm;
+
+        return formatted_time;
+    };
 
 
     // // When the user clicks anywhere outside of the modal, close it
